3ced397ed4a3ef687e45dddb9286143708b3f6d2
Markus Klawun <mklawun@outlook.com>
2026-04-17 23:27:25 -0500

    Created UV project + moved color utils

--- theme-creator.py
+++ theme-creator.py
@@ -1,96 +1,9 @@
-import re
 from PIL import Image
 from statistics import mean
 from math import sqrt
 
-
-def rgb_to_hex(r: int, g: int, b: int) -> str:
-    if not ((r >= 0 and r < 256) and (g >= 0 and g < 256) and (b >= 0 and b < 256)):
-        raise ValueError("Invalid RBG values. Must be 0 <= x < 256")
-    return f'#{r:0>2x}{g:0>2x}{b:0>2x}'
-
-
-def hex_to_rgb(hex_color: str) -> tuple[int, int, int]:
-    hex_color = hex_color.upper()
-    if not re.match(r"[#][0-9A-F]{6}", hex_color):
-        raise ValueError(
-            "Invalid hex color format. Must be #XXXXXX format where X = 0-9 or A-F")
-    hex_color = hex_color.strip("#")
-    r = int(hex_color[0:2], 16)
-    g = int(hex_color[2:4], 16)
-    b = int(hex_color[4:6], 16)
-    return (r, g, b)
-
-
-def rgb_to_hsl(r: int, g: int, b: int) -> tuple[float, float, float]:
-    if not ((r >= 0 and r < 256) and (g >= 0 and g < 256) and (b >= 0 and b < 256)):
-        raise ValueError("Invalid RBG values. Must be 0 <= x < 256")
-    M = max(r, g, b)
-    m = min(r, g, b)
-    C = M - m
-    if C == 0:
-        Hp = 0
-    elif M == r:
-        Hp = ((g - b) / C) % 6
-    elif M == g:
-        Hp = ((b - r) / C) + 2
-    elif M == b:
-        Hp = ((r - g) / C) + 4
-    else:
-        raise ValueError("An unknown ValueError occurred.")
-    H = 60 * Hp
-    L = 0.5 * (M + m) / 255
-    if L == 1 or L == 0:
-        S = 0
-    else:
-        S = abs(C / (1 - (abs(2 * L - 1) - 1))) / 255
-    return (H, S, L)
-
-
-def hsl_to_rgb(h: float, s: float, l: float) -> tuple[int, int, int]:
-    if not ((s >= 0 and s <= 1) and (l >= 0 and l <= 1)):
-        raise ValueError("Values must be valid HSL values.")
-    h = h % 360
-    C = (1 - abs(2 * l - 1)) * s
-    Hp = h / 60
-    X = C * (1 - abs((Hp % 2) - 1))
-    m = l - C / 2
-    if 0 <= Hp < 1:
-        rp, gp, bp = C, X, 0
-    elif 1 <= Hp < 2:
-        rp, gp, bp = X, C, 0
-    elif 2 <= Hp < 3:
-        rp, gp, bp = 0, C, X
-    elif 3 <= Hp < 4:
-        rp, gp, bp = 0, X, C
-    elif 4 <= Hp < 5:
-        rp, gp, bp = X, 0, C
-    elif 5 <= Hp < 6:
-        rp, gp, bp = C, 0, X
-    else:
-        raise ValueError("Could not generate RGB color. Unknown error")
-    r, g, b = round((rp + m) * 255), round((gp + m)
-                                           * 255), round((bp + m) * 255)
-    return (r, g, b)
-
-
-def hsl_to_hex(h: float, s: float, l: float) -> str:
-    r, g, b = hsl_to_rgb(h, s, l)
-    return rgb_to_hex(r, g, b)
-
-
-def hex_to_hsl(hex_color: str) -> tuple[float, float, float]:
-    return rgb_to_hsl(*hex_to_rgb(hex_color))
-
-
-def lighten(color: str, by: float) -> str:
-    h, s, l = hex_to_hsl(color)
-    l += by
-    if l > 1:
-        l = 1
-    elif l < 0:
-        l = 0
-    return hsl_to_hex(h, s, l)
+from color.colors import (hex_to_rgb, hex_to_hsl, hsl_to_hex,
+                          rgb_to_hex, lighten)
 
 
 def create_highlighted_range(dark_color: str, total_colors: int,
@@ -113,13 +26,14 @@ def hsl_lerp(color1: str, color2: str, total_colors: int) -> list[str]:
     for i in range(total_colors):
         h = lerp(color1_hsl[0], color2_hsl[0], i / (total_colors - 1))
         s = lerp(color1_hsl[1], color2_hsl[1], i / (total_colors - 1))
-        l = lerp(color1_hsl[2], color2_hsl[2], i / (total_colors - 1))
-        hsl = hsl_to_hex(h, s, l)
+        lt = lerp(color1_hsl[2], color2_hsl[2], i / (total_colors - 1))
+        hsl = hsl_to_hex(h, s, lt)
         result.append(hsl)
     return result
 
 
-def create_rgb_lerp_range(color1: str, color2: str, total_colors: int) -> list[str]:
+def create_rgb_lerp_range(color1: str, color2: str,
+                          total_colors: int) -> list[str]:
     result = []
     color1_rgb = hex_to_rgb(color1)
     color2_rgb = hex_to_rgb(color2)
@@ -188,7 +102,8 @@ def sort_color_by_dist(lb: HSL, target: HSL, ub: HSL,
     return list(candidates)
 
 
-def generate_ansi_palette(*colors: str, overrides: dict | None = None) -> list[str]:
+def generate_ansi_palette(*colors: str, overrides: dict | None = None
+                          ) -> list[str]:
     result = []
     main_targets = {
         # ansi_name : [lower_bound, target, upper_bound]
